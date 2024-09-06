5530c5ff813898c97b456ac9fe28b58c078c32ef
JesusJimenezE <jesusjinemezetrada@gmail.com>
2024-09-06 13:14:20 -0600

    Empezamos a diseñar la primera pantalla que mostrara un menssaje de bienvenidos, logo y un boton para iniciar sesion este diaseño es un prototipo

--- page/index.php
+++ page/index.php
@@ -1,11 +1,22 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>DENEDIG</title>
+    <link rel="stylesheet" href="./../assets/css/master.css">  <!-- Llamamos al archivo que tiene los estilos -->
 </head>
 <body>
+    <img src="./../assets/resources/img/inicabe.png" style="display: block; margin: 0 auto; width: 100%; height: auto;">
+    <div class="inicio">  <!-- Este caso es para llamar la clase que queremos de los estilos para esta parte -->
+
+        <img src="./../assets/resources/img/DENEDIG ico.png" style="display: block; margin: 0 auto; width: 20%; height: auto;"><br>
     
-</body>
-</html>
+        <p>"Bienvenido a la plataforma de E-mails"</p>
+        <p>"DENEDIG SAD de CV"</p><br>
+        
+        <input class="iniciars"type = "submit" value = "Iniciar sesión" >
+    </div>
+
+
+<?php include "./../includes/footer.php" ?><!-- Llamamos al piede pagina que estara en todas las pantalla -->
