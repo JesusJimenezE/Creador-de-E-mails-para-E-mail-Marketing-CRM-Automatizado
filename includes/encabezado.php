9f29ef11e89173b1cd7b82f1748b725dc404b7ba
JesusJimenezE <jesusjinemezetrada@gmail.com>
2024-09-10 13:00:30 -0600

    Agregamos el encabezado de pagina y una nueva carpeta donde van a ir componentes que vamos a usar, en este caso es la barra de navegacion que nos estara como encabezado y el logo, la barra la concontramos con uno de los diseños de bootstrap

---
+++ includes/encabezado.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DENEDIG</title>
+    <link rel="stylesheet" href="./../assets/css/master.css">  <!-- Llamamos al archivo que tiene los estilos -->
+    <link rel="stylesheet" href="../assets/css/bootstrap.min.css"> <!-- Integramos booststrap en nuestro proyecto para darle estilos a nuestras pantallas -->
+</head>
+<body>
+    <header>
+        <?php include "./../components/barranave.php" ?>
+        <img style="width: 180px" src="./../assets/resources/img/DENEDIGico.png" >
+    </header>
