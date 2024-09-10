d7928779be8b8ee15c87740ce9d2133fe44e1967
JesusJimenezE <jesusjinemezetrada@gmail.com>
2024-09-10 13:04:25 -0600

    Empezamos con la pantalla de contactos que de principo quiero que se visualizen los contactos pero de momento solo tiene un boton que nos manda a la pantalla para agregar nuevo contacto

---
+++ page/contactos.php
@@ -0,0 +1,7 @@
+<?php include "./../includes/encabezado.php" ?> <!-- Llamamos al encabezado de pagina que estara en todas las pantalla -->
+
+
+<a href="./../page/nuecont.php" class="btn btn-primary botoncear"> Agregar contacto </a> 
+
+
+<?php include "./../includes/piepagina.php" ?><!-- Llamamos al piede pagina que estara en todas las pantalla -->
