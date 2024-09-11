8b557325a8916cc8a27cc9d7bbe6e25c57078ea7
JesusJimenezE <jesusjinemezetrada@gmail.com>
2024-09-11 12:58:28 -0600

    Agregamos el componente de carusel para la pantalla de inicio que igual agregamos con la misma extructura, como tambien imagenes de ejemplo

---
+++ components/carousel.php
@@ -0,0 +1,28 @@
+<!-- Carrusel de Bootstrap -->
+<div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+  <!-- Contenedor de los elementos del carrusel -->
+  <div class="carousel-inner">
+    <!-- Primer elemento del carrusel, visible por defecto -->
+    <div class="carousel-item active">
+      <img src="./../assets/resources/img/sepejm.png" class="d-block w-100" alt="Imagen 1">
+    </div>
+    <!-- Segundo elemento del carrusel -->
+    <div class="carousel-item">
+      <img src="./../assets/resources/img/fiessep.png" class="d-block w-100" alt="Imagen 2">
+    </div>
+  </div>
+
+  <!-- Controles del carrusel -->
+  <!-- Botón para pasar a la imagen anterior -->
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  
+  <!-- Botón para pasar a la siguiente imagen -->
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+
---
+++ page/inicio.php
@@ -0,0 +1,7 @@
+<?php include "./../includes/encabezado.php" ?> <!-- Llamamos al encabezado de pagina que estara en todas las pantalla -->
+
+<?php include"./../components/carousel.php"?>
+
+
+
+<?php include "./../includes/piepagina.php" ?><!-- Llamamos al piede pagina que estara en todas las pantalla -->
