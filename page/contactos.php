3be0b572b334b928da1094b996aa78d904bab97c
JesusJimenezE <jesusjinemezetrada@gmail.com>
2024-09-11 12:08:17 -0600

    Agregamos el componente de card, para poder visualizar los contactos agregados previamnete

--- page/contactos.php
+++ page/contactos.php
@@ -3,5 +3,6 @@
 
 <a href="./../page/nuecont.php" class="btn btn-primary botoncear"> Agregar contacto </a> <!-- Boton para que nos dirija a la pantalla que nos ayuda a agregar un nuevo contacto -->
 
+<?php include"./../components/card.php"?>
 
 <?php include "./../includes/piepagina.php" ?><!-- Llamamos al piede pagina que estara en todas las pantalla -->
