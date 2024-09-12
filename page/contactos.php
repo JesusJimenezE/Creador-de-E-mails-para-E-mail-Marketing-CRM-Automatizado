4477324842b831c3ef2120f55a4c98b3100a5a11
JesusJimenezE <jesusjinemezetrada@gmail.com>
2024-09-12 13:04:02 -0600

    Borre todo lo de php para poder hacer el cambio de react

--- page/contactos.php
+++
@@ -1,8 +0,0 @@
-<?php include "./../includes/encabezado.php" ?> <!-- Llamamos al encabezado de pagina que estara en todas las pantalla -->
-
-
-<a href="./../page/nuecont.php" class="btn btn-primary botoncear"> Agregar contacto </a> <!-- Boton para que nos dirija a la pantalla que nos ayuda a agregar un nuevo contacto -->
-
-<?php include"./../components/card.php"?>
-
-<?php include "./../includes/piepagina.php" ?><!-- Llamamos al piede pagina que estara en todas las pantalla -->
