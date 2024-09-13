b33b597f186a1e0a70512c0b69419cfc543598df
JesusJimenezE <jesusjinemezetrada@gmail.com>
2024-09-13 03:27:09 -0600

    Empezamos a editar la barra de navegacion para que quede como lo previsto

--- src/components/Cabe.js
+++ src/components/Cabe.js
@@ -1,23 +1,24 @@
 import React from 'react';
+import './Cabe.module.css';
 import { Nav, NavItem, NavLink } from 'reactstrap'; // Importa los componentes necesarios
 
 export const Cabe = () => {
   return (
-    <div>
+    <div className="nave">
       <Nav>
         <NavItem>
-          <NavLink active href="#">
-            Link
+          <NavLink active href="./../screens/Home.js">
+            Inicio
           </NavLink>
         </NavItem>
         <NavItem>
-          <NavLink href="#">
-            Another Link
+          <NavLink href="./../screens/Contac.js">
+            Contactos
           </NavLink>
         </NavItem>
         <NavItem>
-          <NavLink disabled href="#">
-            Disabled Link
+          <NavLink disabled href="./../screens/Email.js">
+            E-mail
           </NavLink>
         </NavItem>
       </Nav>
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import { Piep } from '../components/Piep';
+import { Cabe } from '../components/Cabe';
 
 export const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      <Cabe/>
+      
+      <Piep/>
+    </div>
   )
 }
 export default Home;
