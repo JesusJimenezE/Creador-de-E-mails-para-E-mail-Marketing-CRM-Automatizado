aad7a62f49d12e2f6c03a3eace02584d86d11afe
JesusJimenezE <jesusjinemezetrada@gmail.com>
2024-09-17 11:22:53 -0600

    Agregamos la pantalla de prefil, de igualmente que la pantalla de e-mail, con la base del maquetado

--- src/screens/Prefil.js
+++ src/screens/Prefil.js
@@ -1,17 +1,44 @@
 import React from 'react'
 import { Cabe } from '../components/Cabe'; 
 import { Piep } from '../components/Piep';
+import { Button, Input, Label, Form, FormGroup } from 'reactstrap';
+import styles from './Prefil.module.css';
 
 export const Prefil = () => {
   return (
     <div>
-      <Cabe /> {/* Añade la barra de navegación aquí */}
-      <div>
-        
-      </div>
+    <Cabe />
+  <div className={styles.PrefPage}> {/* Clase para centrar la página */}
+    
+      <div className={styles.FormContainer}> {/* Aplicamos la clase FormContainer aquí */}
+        <Form>
+          <FormGroup>
+            <Label for="asunto">Nombre:</Label>
+            <Input id="nombre" name="nombre" type="text" />
+          </FormGroup>
 
-      <Piep/>
+          <FormGroup>
+            <Label for="conte">Correo:</Label>
+            <Input id="correo" name="correo" type="email" />
+          </FormGroup>
+
+          <FormGroup>
+            <Label for="conte">Numero:</Label>
+            <Input id="numero" name="numero" type="number" />
+          </FormGroup>
+
+          <FormGroup>
+            <Label for="conte">Contraseña:</Label>
+            <Input id="contraseña" name="contraseña" type="password" />
+          </FormGroup>
+
+          <Button className={styles.guar}>Guardar cambios</Button>
+        </Form>
     </div>
+    
+  </div>
+  <Piep />
+  </div>
   )
 }
 export default Prefil;
