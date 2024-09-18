855f8502d38ff730ea5cdf0a099c8a01024da2c2
JesusJimenezE <jesusjinemezetrada@gmail.com>
2024-09-18 11:51:06 -0600

    Agregamos ya tres imagenes de ejemplo para el carusel

--- src/components/Carusel.js
+++ src/components/Carusel.js
@@ -11,19 +11,19 @@ export const Carusel = () => {
             altText: 'Slide 1',
             caption: 'Slide 1',
             key: 1,
-            src: 'https://picsum.photos/seed/picsum/1200/600'
+            src: 'https://content.clara.es/medio/2024/06/05/frases-motivadoras-de-trabajo-duro_00000000_3853915f_240605120108_1200x630.jpg'
           },
           {
             altText: 'Slide 2',
             caption: 'Slide 2',
             key: 2,
-            src: 'https://picsum.photos/id/456/1200/600'
+            src: 'https://assets.asana.biz/transform/c693264d-d11e-404d-bed6-fd19df1e919a/inline-leadership-team-motivational-quotes-6-es-2x'
           },
           {
             altText: 'Slide 3',
             caption: 'Slide 3',
             key: 3,
-            src: 'https://picsum.photos/id/678/1200/600'
+            src: 'https://static.guiainfantil.com/media/22796/frases-motivacion-enero-ninos.jpg'
           }
         ]}
       />
