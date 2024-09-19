060d92553d49d6c647b8f60fc7091560418cf3e9
JesusJimenezE <jesusjinemezetrada@gmail.com>
2024-09-19 12:52:09 -0600

    Agregamos comentarios en el codigo de Nuecont donde va ser que se agregen los contactos

--- src/screens/Nuecont.js
+++ src/screens/Nuecont.js
@@ -1,30 +1,32 @@
-import React from 'react'
+import React from 'react';
 import { Cabe } from '../components/Cabe'; 
 import { Piep } from '../components/Piep';
 import { Button, Input, Label, Form, FormGroup } from 'reactstrap';
 import styles from './Nuecont.module.css'; 
 
-
 export const Nuecont = () => {
   return (
     <div>
-      <Cabe />
-    <div className={styles.NuevoPage}> 
-      
+      <Cabe /> {/* Componente de cabecera */}
+
+      <div className={styles.NuevoPage}> 
         <div className={styles.FormContainer}> 
           <Form>
+            {/* Campo para el nombre */}
             <FormGroup>
-              <Label for="asunto">Nombre:</Label>
+              <Label for="nombrec">Nombre:</Label>
               <Input id="nombrec" name="nombre" type="text" />
             </FormGroup>
 
+            {/* Campo para la edad */}
             <FormGroup>
-              <Label for="conte">Edad:</Label>
+              <Label for="edad">Edad:</Label>
               <Input id="edad" name="edad" type="number" />
             </FormGroup>
 
+            {/* Selector para género */}
             <FormGroup>
-              <Label for="exampleSelect">Genero:</Label>
+              <Label for="genero">Género:</Label>
               <Input id="genero" name="select" type="select">
                 <option>Masculino</option>
                 <option>Femenino</option>
@@ -32,30 +34,34 @@ export const Nuecont = () => {
               </Input>
             </FormGroup>
 
+            {/* Campo para correo */}
             <FormGroup>
-            <Label for="conte">Correo:</Label>
-            <Input id="correoc" name="correo" type="email" />
-          </FormGroup>
+              <Label for="correoc">Correo:</Label>
+              <Input id="correoc" name="correo" type="email" />
+            </FormGroup>
 
-          <FormGroup>
-            <Label for="conte">Numero telefonico:</Label>
-            <Input id="numeroc" name="numero" type="number" />
-          </FormGroup>
+            {/* Campo para número telefónico */}
+            <FormGroup>
+              <Label for="numeroc">Número telefónico:</Label>
+              <Input id="numeroc" name="numero" type="number" />
+            </FormGroup>
 
-          <FormGroup>
-              <Label for="conte">Dirrecion:</Label>
+            {/* Campo para dirección */}
+            <FormGroup>
+              <Label for="dirrecion">Dirección:</Label>
               <Input id="dirrecion" name="text" type="textarea" />
             </FormGroup>
 
+            {/* Botones para guardar y limpiar */}
             <Button className={styles.guar}>Guardar</Button>
             <Button className={styles.lim}>Limpiar</Button>
           </Form>
+        </div>
       </div>
-      
+
+      <Piep /> {/* Componente de pie de página */}
     </div>
-    <Piep />
-    </div>
-  )
+  );
 }
 
 export default Nuecont;
