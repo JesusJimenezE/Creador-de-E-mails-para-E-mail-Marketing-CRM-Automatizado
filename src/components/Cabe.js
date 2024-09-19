ffad0faa9a919f395977cc53c3c24f73c0151ca4
JesusJimenezE <jesusjinemezetrada@gmail.com>
2024-09-19 10:28:23 -0600

    Empezamos a agregar los comentario para explicar partes del codigo

--- src/components/Cabe.js
+++ src/components/Cabe.js
@@ -1,15 +1,19 @@
 import React from 'react';
-import { Nav, NavItem } from 'reactstrap';
-import { NavLink as RouterNavLink } from 'react-router-dom';
-import styles from './Cabe.module.css'; 
-import logo from './../assets/img/DENEDIGico.png';
+import { Nav, NavItem } from 'reactstrap'; //importamos las herramientas de bootstrap que se van a usar
+import { NavLink as RouterNavLink } from 'react-router-dom'; //importamos NavLink y le cambiamos el nombre para usarlo como RouterNavLink
+import styles from './Cabe.module.css'; //importamos los estilos personalizados desde un archivo CSS
+import logo from './../assets/img/DENEDIGico.png'; //importamos el logo desde la carpeta de imágenes
 
 export const Cabe = () => {
-  return ( // usamos lo div para darle los diseños de la pagina en ciertas partes, por lo mismo le agregamos clases y estilos donde le ponemos el nombre de esa parte
+  return ( 
+    // usamos el div para darle los diseños de la página en ciertas partes, por lo mismo le agregamos clases y estilos donde le ponemos el nombre de esa parte
     <div className={styles.headerContainer}> 
+      
+      {/* Sección de navegación principal */}
       <div className={styles.nave}>
         <Nav className={styles.leftNav}>
           <NavItem>
+            {/* Usamos RouterNavLink para el redireccionamiento sin recargar la página */}
             <RouterNavLink to="/home" className={styles['nav-link']}>
               Inicio
             </RouterNavLink>
@@ -26,6 +30,7 @@ export const Cabe = () => {
           </NavItem>
         </Nav>
         
+        {/* Sección de navegación secundaria (parte derecha) */}
         <div className={styles.rightNav}>
           <NavItem>
             <RouterNavLink to="/perfil" className={styles['nav-link']}>
@@ -39,8 +44,10 @@ export const Cabe = () => {
           </NavItem>
         </div>
       </div>
+      
+      {/* Contenedor del logo */}
       <div className={styles['logo-container']}>
-        <img src={logo} alt="logo" className={styles['logo-image']} />
+        <img src={logo} alt="logo" className={styles['logo-image']} /> {/* Mostramos el logo importado */}
       </div>
     </div>
   );
