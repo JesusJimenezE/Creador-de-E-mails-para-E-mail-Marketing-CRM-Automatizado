1cde85e1761e9f2a50f2faf20eeecc1c7cf31ed4
JesusJimenezE <jesusjinemezetrada@gmail.com>
2024-09-19 11:57:13 -0600

    Agregamos comentarios en el codigo de de App, que es la pagina donde estan las rutas y  es l aprimera pantalla que se veria

--- src/App.js
+++ src/App.js
@@ -1,57 +1,67 @@
 import React from 'react';
-import { Button } from 'reactstrap';
-import { Routes, Route, useNavigate } from 'react-router-dom'; 
-import './App.css';
-import inicabe from './assets/img/inicabe.png';
-import DENEDIG from './assets/img/DENEDIGico.png';
-import { Piep } from './components/Piep';
-import Login from './screens/Login'; 
-import Home from './screens/Home'; 
-import Contac from './screens/Contac'; 
-import Email from './screens/Email'; 
-import Prefil from './screens/Prefil';
-import Nuecont from './screens/Nuecont'; 
-import Nuepre from './screens/Nuepre';
-import Infcont from './screens/Infcont';
+import { Button } from 'reactstrap'; // Importamos el componente Button de Reactstrap para usar botones con estilo
+import { Routes, Route, useNavigate } from 'react-router-dom'; // Importamos las herramientas de navegación y manejo de rutas
+import './App.css'; // Importamos los estilos de la aplicación
+import inicabe from './assets/img/inicabe.png'; // Imagen para la parte superior
+import DENEDIG from './assets/img/DENEDIGico.png'; // Logo de DENEDIG
+import { Piep } from './components/Piep'; // Importamos el componente del pie de página
+import Login from './screens/Login'; // Pantalla de inicio de sesión
+import Home from './screens/Home'; // Pantalla de inicio
+import Contac from './screens/Contac'; // Pantalla de contactos
+import Email from './screens/Email'; // Pantalla de E-mail
+import Prefil from './screens/Prefil'; // Pantalla del perfil
+import Nuecont from './screens/Nuecont'; // Pantalla para agregar nuevo contacto
+import Nuepre from './screens/Nuepre'; // Pantalla para agregar nuevo perfil
+import Infcont from './screens/Infcont'; // Pantalla con información del contacto
 
 function App() {
-  const navigate = useNavigate();
+  const navigate = useNavigate(); // Hook para la navegación entre páginas
 
+  // Función para redirigir a la página de login cuando se hace clic en el botón
   const handleLoginClick = () => {
     navigate('/login');
   };
 
   return (
+    // Definimos las rutas de la aplicación
     <Routes>
+      {/* Ruta para la página principal (home) */}
       <Route
         path="/"
         element={
           <div className="App">
+            {/* Imagen superior de la cabecera */}
             <img src={inicabe} alt="Parte de arriba" className="header-image" />
             <div>
+              {/* Logo de DENEDIG */}
               <img src={DENEDIG} alt="Logo" className="logo-image" />
             </div>
+            {/* Mensajes de bienvenida */}
             <div>"Bienvenido a la plataforma de E-mails"</div>
             <div>"DENEDIG SAD de CV"</div>
             <div>
+              {/* Botón para iniciar sesión */}
               <Button color="primary" onClick={handleLoginClick}>Iniciar sesión</Button>
             </div>
+            {/* Componente del pie de página */}
             <Piep />
           </div>
         }
       />
-      <Route path="/login" element={<Login />} />
-      <Route path="/home" element={<Home />} />
-      <Route path="/contac" element={<Contac />} />
-      <Route path="/email" element={<Email />} />
-      <Route path="/perfil" element={<Prefil />} />
-      <Route path="/nuecont" element={<Nuecont />} /> 
-      <Route path="/app" element={<App />} />
-      <Route path="/nuepre" element={<Nuepre />} />
-      <Route path="/infcont" element={<Infcont />} />
-    </Routes>
       
+      {/* Definimos las rutas para las diferentes páginas */}
+      <Route path="/login" element={<Login />} /> {/* Ruta para la pantalla de login */}
+      <Route path="/home" element={<Home />} /> {/* Ruta para la pantalla de inicio */}
+      <Route path="/contac" element={<Contac />} /> {/* Ruta para la pantalla de contactos */}
+      <Route path="/email" element={<Email />} /> {/* Ruta para la pantalla de emails */}
+      <Route path="/perfil" element={<Prefil />} /> {/* Ruta para la pantalla de perfil */}
+      <Route path="/nuecont" element={<Nuecont />} /> {/* Ruta para la pantalla de nuevo contacto */}
+      <Route path="/app" element={<App />} /> {/* Ruta para volver a la app */}
+      <Route path="/nuepre" element={<Nuepre />} /> {/* Ruta para la pantalla de nuevo perfil */}
+      <Route path="/infcont" element={<Infcont />} /> {/* Ruta para la información de contacto */}
+    </Routes>
   );
 }
 
-export default App;
+export default App; // Exportamos el componente App como predeterminado
+
