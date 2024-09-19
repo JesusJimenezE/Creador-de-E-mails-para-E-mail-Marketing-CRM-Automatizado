11ef3a92aa2dd778b1040bf3596205aeaf071649
JesusJimenezE <jesusjinemezetrada@gmail.com>
2024-09-19 12:30:17 -0600

    Agregamos comentarios en el codigo de homee o pantalla principal

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,20 +1,24 @@
 import React from 'react';
-import { Cabe } from '../components/Cabe'; 
-import { Piep } from '../components/Piep';
-import { Carusel } from '../components/Carusel';
+import { Cabe } from '../components/Cabe'; // Importamos el componente de la cabecera
+import { Piep } from '../components/Piep'; // Importamos el componente del pie de página
+import { Carusel } from '../components/Carusel'; // Importamos el componente del carrusel de imágenes
 
 export const Home = () => {
   return (
     <div>
+      {/* Componente de la cabecera */}
       <Cabe /> 
 
+      {/* Componente del carrusel que muestra imágenes o contenido destacado */}
       <div>
-        <Carusel/>
+        <Carusel />
       </div>
 
-      <Piep/>
+      {/* Componente del pie de página */}
+      <Piep />
     </div>
   );
 };
 
-export default Home;
+export default Home; // Exportamos el componente como predeterminado
+
