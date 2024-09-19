f6f6f67e05892efb5122bc89ea5ae0bcebb4e798
JesusJimenezE <jesusjinemezetrada@gmail.com>
2024-09-19 11:12:15 -0600

    Agregamos comentarios en el codigo del card

--- src/components/CustomCard.js
+++ src/components/CustomCard.js
@@ -1,32 +1,37 @@
 import React from 'react';
-import { Card as ReactstrapCard, CardBody, CardTitle, CardText, Button } from 'reactstrap';
-import styles from './CustomCard.module.css'; // Importa el archivo de estilos
-import { useNavigate } from 'react-router-dom'; 
+import { Card as ReactstrapCard, CardBody, CardTitle, CardText, Button } from 'reactstrap'; // Importamos los componentes de Reactstrap
+import styles from './CustomCard.module.css'; // Importa el archivo de estilos personalizados
+import { useNavigate } from 'react-router-dom'; // Hook para la navegación
 
 export const CustomCard = () => {
 
-    const navigate = useNavigate(); 
+    const navigate = useNavigate(); // Usamos el hook `useNavigate` para redireccionar a otras rutas
 
+    // Función para manejar el evento al hacer clic en el botón
     const handleSubmit = () => {
-      navigate('/infcont'); 
+      navigate('/infcont'); // Navega a la página de información de contacto
     };
 
     return (
-        <div className={styles['card-container']}> {/* Aplica la clase del contenedor */}
-            <ReactstrapCard className={styles['card']}> {/* Aplica la clase al card */}
+        // Contenedor principal de todas las tarjetas, aplicamos la clase CSS correspondiente
+        <div className={styles['card-container']}> 
+            
+            {/* Tarjeta individual con contenido de contacto */}
+            <ReactstrapCard className={styles['card']}> 
                 <CardBody>
                     <CardTitle tag="h5">
-                        Contacto
+                        Contacto {/* Título de la tarjeta */}
                     </CardTitle>
                     <CardText>
-                        Alguna información del contacto
+                        Alguna información del contacto {/* Texto descriptivo dentro de la tarjeta */}
                     </CardText>
-                    <Button className={styles.ver} onClick={handleSubmit}>
-                        Ver mas
+                    <Button className={styles.ver} onClick={handleSubmit}> {/* Botón que redirige a más información */}
+                        Ver más
                     </Button>
                 </CardBody>
             </ReactstrapCard>
 
+            {/* Repetimos la estructura de la tarjeta para crear múltiples tarjetas */}
             <ReactstrapCard className={styles['card']}>
                 <CardBody>
                     <CardTitle tag="h5">
@@ -82,6 +87,8 @@ export const CustomCard = () => {
                     </Button>
                 </CardBody>
             </ReactstrapCard>
+
         </div>
     );
 };
+
