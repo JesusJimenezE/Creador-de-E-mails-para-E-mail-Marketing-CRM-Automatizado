bc9ef88941789d1f0c37a2aa0b616730c96988d6
JesusJimenezE <jesusjinemezetrada@gmail.com>
2024-09-19 12:19:23 -0600

    Agregamos comentarios en el codigo de Email

--- src/screens/Email.js
+++ src/screens/Email.js
@@ -1,27 +1,34 @@
 import React from 'react';
-import { Button, Input, Label, Form, FormGroup } from 'reactstrap';
-import { Cabe } from '../components/Cabe';
-import { Piep } from '../components/Piep';
-import styles from './Email.module.css'; 
+import { Button, Input, Label, Form, FormGroup } from 'reactstrap'; // Importamos los componentes necesarios de Reactstrap
+import { Cabe } from '../components/Cabe'; // Importamos el componente de la cabecera
+import { Piep } from '../components/Piep'; // Importamos el componente del pie de página
+import styles from './Email.module.css'; // Importamos los estilos específicos para esta página
 
 export const Email = () => {
   return (
     <div>
+      {/* Componente de la cabecera */}
       <Cabe />
-    <div className={styles.EmailPage}>
       
+      {/* Contenedor principal de la página de envío de emails */}
+      <div className={styles.EmailPage}>
+        
+        {/* Contenedor para el formulario */}
         <div className={styles.FormContainer}> 
           <Form>
+            {/* Campo de texto para el asunto del email */}
             <FormGroup>
               <Label for="asunto">Asunto:</Label>
               <Input id="asunto" name="asunto" type="text" />
             </FormGroup>
 
+            {/* Campo para el contenido del email */}
             <FormGroup>
               <Label for="conte">Contenido:</Label>
               <Input id="conte" name="text" type="textarea" />
             </FormGroup>
 
+            {/* Selector para elegir la audiencia */}
             <FormGroup>
               <Label for="exampleSelect">Audiencia:</Label>
               <Input id="exampleSelect" name="select" type="select">
@@ -31,20 +38,23 @@ export const Email = () => {
               </Input>
             </FormGroup>
 
+            {/* Campo para subir un archivo adjunto */}
             <FormGroup>
               <Label for="exampleFile">Archivo:</Label>
               <Input id="exampleFile" name="file" type="file" />
             </FormGroup>
 
+            {/* Botones para enviar el formulario y limpiar los campos */}
             <Button className={styles.env}>Enviar</Button>
             <Button className={styles.lim}>Limpiar</Button>
           </Form>
+        </div>
       </div>
-      
-    </div>
-    <Piep />
+
+      {/* Componente del pie de página */}
+      <Piep />
     </div>
   );
 };
 
-export default Email;
+export default Email; // Exportamos el componente como predeterminado
