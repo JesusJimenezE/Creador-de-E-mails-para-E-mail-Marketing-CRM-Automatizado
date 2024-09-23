c6cf34d60173d3f337114710fa2b6d484bb019f3
JesusJimenezE <jesusjinemezetrada@gmail.com>
2024-09-23 10:03:59 -0600

    Dejamos la pantalla de index para poder seguir en la configuracion

--- src/index.js
+++ src/index.js
@@ -1,19 +1,21 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router } from 'react-router-dom';
-import { FirebaseAppProvider } from 'reactfire'; // Cambia a FirebaseAppProvider con mayúscula
+import { FirebaseAppProvider } from 'reactfire'; 
 import fireBaseConfig from './components/firebaseconfig';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Router>
-      <FirebaseAppProvider firebaseConfig={fireBaseConfig}> {/* Capitalización corregida */}
+      <FirebaseAppProvider firebaseConfig={fireBaseConfig}> 
+        <Suspense fallback={'Conectando la app...'}>
         <App />
+        </Suspense>
       </FirebaseAppProvider>
     </Router>
   </React.StrictMode>
