c5c18011329a368c7ea5e2e7c74f5dda1a498342
JesusJimenezE <jesusjinemezetrada@gmail.com>
2024-09-23 09:38:53 -0600

    Agregamos la primera configuracion de firebase

--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css'; 
+import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter as Router } from 'react-router-dom'; 
+import { BrowserRouter as Router } from 'react-router-dom';
+import { firebaseappProvider } from 'reactfire'
+import fireBaseConfig from './components/firebaseconfig'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router> 
-      <App />
+    <Router>
+      <firebaseappProvider fireBaseConfig={}>
+        <App />
+      </firebaseappProvider>
     </Router>
   </React.StrictMode>
 );
