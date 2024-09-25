3208b550d7ac4c876fa2f1416dbe562d6663d1e6
JesusJimenezE <jesusjinemezetrada@gmail.com>
2024-09-25 10:12:16 -0600

    Agregamos la estructura de los datos para que se pueda guardar en nuestra coleccion

--- src/screens/Nuecont.js
+++ src/screens/Nuecont.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import { Cabe } from '../components/Cabe'; 
+import React, { useState } from 'react';
+import { Cabe } from '../components/Cabe';
 import { Piep } from '../components/Piep';
 import { Button, Input, Label, Form, FormGroup } from 'reactstrap';
-import styles from './Nuecont.module.css'; 
+import styles from './Nuecont.module.css';
 import { getFirestore, collection, addDoc, getDocs, doc, deleteDoc, getDoc, setDoc } from 'firebase/firestore'
 
 export const Nuecont = () => {
@@ -13,54 +13,68 @@ export const Nuecont = () => {
     genero: '',
     correo: '',
     numero: '',
-    dirrecion: ''
+    direccion: ''
+  }
+
+  const [user, setUser] = useState(valorInicial)
+
+  const capturarInputs = (e) => {
+    const { name, value } = e.target;
+    setUser({ ...user, [name]: value })
+  }
+
+  const guardarDatos = async (e) => {
+    e.preventDefault();
+    console.log(user)
+    setUser({ ...valorInicial })
   }
 
   return (
     <div>
       <Cabe /> {/* Componente de cabecera */}
 
-      <div className={styles.NuevoPage}> 
-        <div className={styles.FormContainer}> 
-          <Form>
+      <div className={styles.NuevoPage}>
+        <div className={styles.FormContainer}>
+          <Form onSubmit={guardarDatos}>
             {/* Campo para el nombre */}
             <FormGroup>
               <Label for="nombrec">Nombre:</Label>
-              <Input id="nombrec" name="nombre" type="text" />
+              <Input id="nombrec" name="nombre" type="text" onChange={capturarInputs} value={user.nombre} />
             </FormGroup>
 
             {/* Campo para la edad */}
             <FormGroup>
               <Label for="edad">Edad:</Label>
-              <Input id="edad" name="edad" type="number" />
+              <Input id="edad" name="edad" type="number" onChange={capturarInputs} value={user.edad} />
             </FormGroup>
 
             {/* Selector para género */}
             <FormGroup>
               <Label for="genero">Género:</Label>
-              <Input id="genero" name="select" type="select">
-                <option>Masculino</option>
-                <option>Femenino</option>
-                <option>Otros</option>
+              <Input id="genero" name="genero" type="select" onChange={capturarInputs} value={user.genero}>
+                <option value="">Seleccione una opción</option>
+                <option value="Masculino">Masculino</option>
+                <option value="Femenino">Femenino</option>
+                <option value="Otros">Otros</option>
               </Input>
             </FormGroup>
 
             {/* Campo para correo */}
             <FormGroup>
               <Label for="correoc">Correo:</Label>
-              <Input id="correoc" name="correo" type="email" />
+              <Input id="correoc" name="correo" type="email" onChange={capturarInputs} value={user.correo} />
             </FormGroup>
 
             {/* Campo para número telefónico */}
             <FormGroup>
               <Label for="numeroc">Número telefónico:</Label>
-              <Input id="numeroc" name="numero" type="number" />
+              <Input id="numeroc" name="numero" type="number" onChange={capturarInputs} value={user.numero} />
             </FormGroup>
 
             {/* Campo para dirección */}
             <FormGroup>
-              <Label for="dirrecion">Dirección:</Label>
-              <Input id="dirrecion" name="text" type="textarea" />
+              <Label for="direccion">Dirección:</Label>
+              <Input id="direccion" name="direccion" type="textarea" onChange={capturarInputs} value={user.direccion} />
             </FormGroup>
 
             {/* Botones para guardar y limpiar */}
