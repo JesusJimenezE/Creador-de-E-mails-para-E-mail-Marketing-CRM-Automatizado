0637a3fc9a1181aa0459262b63c7411e92888631
JesusJimenezE <jesusjinemezetrada@gmail.com>
2025-01-07 11:47:23 -0600

    Corregimos un error

--- backend/router/email.routes.js
+++ backend/router/email.routes.js
@@ -1,8 +1,9 @@
-const express = require('express'); // Importa el módulo Express, que se utiliza para crear y gestionar rutas en la aplicación
-const router = express.Router(); // Crea una instancia del enrutador de Express para definir rutas de manera modular
-const controller = require('../controllers/emailController'); // Importa el controlador que contiene la lógica para manejar las solicitudes relacionadas con el envío de correos
+const express = require('express'); // Importa el módulo Express
+const router = express.Router(); // Crea un enrutador de Express
+const controller = require('../controllers/emailController'); // Importa el controlador
 
-// Define una ruta POST 
-router.post('/enviar-correo', controller.post);
+// Cambia `controller.post` por `controller.sendEmail`
+router.post('/enviar-correo', controller.sendEmail);
 
-module.exports = router; // Exporta el enrutador para que pueda ser utilizado en otros archivos de la aplicación
+module.exports = router; // Exporta el enrutador
+
